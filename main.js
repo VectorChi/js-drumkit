6f6c5c7647060ab3ee31fe8771e678a1d4d838fd
Adimchi <adimchisylvester2@gmail.com>
2023-04-12 13:19:00 +0100

    drumkit first push

---
+++ main.js
@@ -0,0 +1,15 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key=${e.key}]`);
+  const key = document.querySelector(`.key[data-key=${e.key}]`);
+  if (!audio) return; //stop function if no audio
+  audio.currentTime = 0; //rewind to the start every click
+  audio.play();
+  key.classList.add("playing");
+}
+function removeTransition(e) {
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
